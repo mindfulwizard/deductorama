c4f254c5df06a057eeaf721926ff6059199f9cf2
Isaac Ratner <isaacratner@gmail.com>
2015-07-21 17:01:49 -0400

    colors change on click

--- deductorama.js
+++ deductorama.js
@@ -1,20 +1,24 @@
 var app = angular.module('deductorama', []);
 var counter;
+var columnCounter;
 
 app.controller('MainCtrl', function ($scope, CircleFactory, ColumnFactory) {
 	$scope.guesses = [];
 	$scope.colorsArr = [];
+	//$scope.rows = [1,1,1,1];
+	$scope.rows = [1,2,3,4];
+	$scope.columns = [1];
 
 
-	$scope.colorCycler = function() {
+	$scope.colorCycler = function($index) {
 			var availableColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 			counter = counter || 0;
 			if(counter === availableColors.length) {
 				counter = 0;
 			}
-			$scope.colorsArr[0] = availableColors[counter];
+			$scope.colorsArr[$index] = availableColors[counter];
 			counter++;
-			console.log($scope.colorsArr);
+			console.log("colorsArr is", $scope.colorsArr);
 		};
 	
 
@@ -23,7 +27,12 @@ app.controller('MainCtrl', function ($scope, CircleFactory, ColumnFactory) {
 	};
 
 
-	$scope.showNextColumn = ColumnFactory.showNextColumn;
+	$scope.showNextColumn = function() {
+		columnCounter = columnCounter || 2;
+		console.log(columnCounter);
+		$scope.columns.push(columnCounter);
+		columnCounter++;
+	}
 });
 
 
@@ -42,6 +51,7 @@ app.factory('ColumnFactory', function() {
 	return {
 		showNextColumn: function() {
 			console.log('angular working');
+			$scope.columns.push(1)
 		}
 	}
 });
@@ -54,6 +64,8 @@ app.factory('ColumnFactory', function() {
 //   [[ this div will basically call push on $scope guesses]]
 // </div>
 
+
+//on click, push something into columns array
 
 
 
